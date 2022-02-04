6182867e649e82cba92521bb213e9411c0cd2a9f
fini04001 <92377261+fini04001@users.noreply.github.com>
2022-02-04 15:52:34 +0100

    Add files via upload

---
+++ Gladtyp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+namespace Gladtyp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            initArray();
+        }
+
+        private static void initArray()
+        {
+            int[] arr = new int[10];
+            Random rnd = new Random ();
+            int tmp;
+            
+            for (int i = 0; i <arr.Length; i++)
+            {
+                tmp = rnd.Next(11);
+                while (isdup(tmp,arr))
+                {
+                    
+                }
+            }
+    
+        }
+        private static bool isdup(int tmp, int[] arr)
+        {
+            foreach (var item in arr)
+            {
+                
+            }
+        }
+    }
+}
